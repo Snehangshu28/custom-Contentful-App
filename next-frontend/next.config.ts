f6861bb9012f1511aabb4003683c29f2d27650f8
snehangshu <snehangshu1228@gmail.com>
2025-06-22 06:22:36 +0530

    init commit

--- next-frontend/next.config.ts
+++ next-frontend/next.config.ts
@@ -18,13 +18,9 @@ const nextConfig: NextConfig = {
     ],
   },
   typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has TypeScript errors.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
